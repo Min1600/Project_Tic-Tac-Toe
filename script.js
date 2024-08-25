2560e147ed1a2a02c2994cf6379a341e36db37be
Min1600 <aminhassan00@gmail.com>
2024-08-25 22:01:05 +0100

    Game Working !

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
-/*const btn = document.querySelectorAll("button")
+const btn = document.querySelectorAll(".game")
+const resetBtn = document.querySelector(".reset")
 
-window.addEventListener("click", (event) => {
-    gameBoard.click() 
+window.addEventListener("click", (event) => { 
     for(const obj in btn){
    if (btn[obj].id === event.target.id){
-     btn[obj].innerText = gameBoard.click().tile
-  }}})    
-const div = document.querySelector("h2")*/
-const gameBoard = (function (){ 
+    btn[obj].disabled = true;
+     btn[obj].innerText = gameBoard.click(event.target.id).tile, 
+     gameBoard.winner()
+  }}})   
+ 
+const gameBoard = (function (){  
+
+const declare = document.querySelector(".declare")
 const p1Wins = "Player 1 Wins"  
-const p2Wins = "Player 2 Wins"
+const p2Wins = "Player 2 Wins" 
 let tile = ""
-let currentPlayer = "p1"
+let currentPlayer = "p1" 
 let p1Tiles = []
 let p2Tiles = []
+let msg = "Player 2's Turn"
 
 const player1 = () =>{
 let score = 0
-let msg = "Player 2's turn"
+ msg = "Player 1's Turn"
 tile = "X"
 return {tile, score, msg}
-}
+} 
 
 const player2 = () =>{
     let score = 0
-    let msg = "Player 1's turn"
+     msg = "Player 2's Turn"
     tile = "O"
     return {tile, score, msg}
     }
 
-const click = (num) =>{
+const click = (event) =>{
 if(currentPlayer === "p1"){
     currentPlayer = currentPlayer === "p1" ? "p2": "p1"
-    p1Tiles.push(num)
-    console.log(p1Tiles)
+    p1Tiles.push(Number(event))
+    declare.innerText = msg
     return player1()
 }else
 currentPlayer = currentPlayer === "p1" ? "p2": "p1"
-p2Tiles.push(num)
-console.log(p2Tiles)
+p2Tiles.push(Number(event))
+declare.innerText = msg
 return player2()
 }
 
@@ -51,43 +56,33 @@ const outcomes = {
     six: [3,6,9],
     seven: [1,5,9],
     eight: [3,5,7],
-   }
+   }  
 
 const winner = () =>{
     for(const obj in outcomes){
-if(outcomes[obj].every((num) => p1Tiles.includes(num))){
-     console.log(p1Wins)
+    if(outcomes[obj].every((num) => p1Tiles.includes(num))){
+    return declare.innerText = p1Wins, 
+           btn.forEach((item) => item.disabled = true)
 }else if(outcomes[obj].every((num) => p2Tiles.includes(num))){
-    console.log(p2Wins)
-    }else if (p1Tiles.length === 5){
-        console.log("It's a Draw!")
-    }else{
-        return ""
-    }}}
+    return declare.innerText = p2Wins,
+     btn.forEach((item) => item.disabled = true)
+    }
+    }if(p1Tiles.length === 5){
+        return declare.innerText = "It's a Draw!" 
+    }}
 
-return{click, winner}
-})()
+    const reset =  () => {
+        tile = ""
+        currentPlayer = "p1" 
+        p1Tiles = []
+        p2Tiles = []
+        msg = "Player 2's Turn"
+      declare.innerText = "Player 1's Turn"
+      btn.forEach((item) => item.innerText = "");
+      btn.forEach((item) => item.disabled = false)
+    }
 
-const displayController = (function () {
-   
+return{click, winner, msg, reset} 
+})() 
 
-
-})()
-
-
-
-
-
-console.log(gameBoard.click(1), gameBoard.winner())
-console.log(gameBoard.click(5), gameBoard.winner())
-console.log(gameBoard.click(2), gameBoard.winner())
-console.log(gameBoard.click(9), gameBoard.winner())
-console.log(gameBoard.click(3), gameBoard.winner())
-
-
-
-
-
-
-
-
+resetBtn.addEventListener("click", () => gameBoard.reset())
